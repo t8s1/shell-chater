f340ff1ed86f1dee583ea1773253ed6e9c9fbaf1
t8s1 <108776922+t8s1@users.noreply.github.com>
2022-10-27 07:59:08 +0800

    Add files via upload

---
+++ client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Shell Chat client"
+read -n 1 -p "Do you want to start chat [Y/n] > " yesOrNo
+printf "\n"
+
+#主代码
+if [ ${yesOrNo} == "Y" ]
+then
+	read -p "Please enter the service IP > " ip
+	read -p "Please enter the service port number > " port
+	printf "\n"
+	echo "Connect to the server now"
+	nc ${ip} ${port}
+fi
+
+#结束
+echo "Bye"
---
+++ server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Shell Chat Server"
+read -n 1 -p "Do you want to start chat-server [Y/n] > " yesOrNo
+printf "\n"
+
+#主代码
+if [ ${yesOrNo} == "Y" ]
+then
+	read -p "Please enter the service port number > " port
+	printf "\n"
+	echo "Do not enter words, use Ctrl+C to shut down the server"
+	nc -lvp ${port}
+fi
+
+#结束
+echo "Bye"
